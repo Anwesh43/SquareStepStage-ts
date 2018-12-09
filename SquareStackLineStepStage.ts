af1ff23247966d8f089edc431068da921e70c4e5
Anwesh43 <anweshthecool0@gmail.com>
2018-12-09 16:07:56 +0530

    adding context drawing method

--- SquareStackLineStepStage.ts
+++ SquareStackLineStepStage.ts
@@ -5,6 +5,9 @@ const h : number = window.innerHeight
 const scGap : number = 0.05
 const scDiv : number = 0.51
 const color : string = "#01579B"
+const sizeFactor : number = 2.3
+const strokeFactor : number = 90
+const lineSizeFactor : number = 0.6
 
 const divideScale : Function = (scale : number, i : number, n : number) : number =>Math.min(1/n, Math.max(0, scale - i/n)) * n
 const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
@@ -13,3 +16,33 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
       return (1 - k) / a + k / b
 }
 const updateScale : Function = (scale : number, dir : number, a : number, b : number) => mirrorValue(scale, a, b) * scGap * dir
+
+const drawSSLSNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = w / (nodes + 1)
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const size : number = gap / sizeFactor
+    context.strokeStyle = color
+    context.lineWidth = Math.min(w, h) / strokeFactor
+    context.lineCap = 'round'
+    const lineSize : number = size * lineSizeFactor
+    const offsetX : number = size * 0.9 - lineSize
+    const yGap = 2 * size / (lines / 2 + 1)
+    context.save()
+    context.translate(gap * (i + 1), h/2)
+    context.rotate(Math.PI/2 * sc2)
+    context.strokeRect(-size, -size, 2 * size, 2 * size)
+    for(var j = 0; j < lines; j++) {
+        const sc : number = divideScale(sc1, j, lines)
+        const x = -size + size * Math.floor(j / 4) + offsetX
+        const y =
+        context.save()
+        context.translate(x, yGap * ((j % 4) + 1))
+        context.beginPath()
+        context.moveTo(0, 0)
+        context.lineTo(lineSize * sc, 0)
+        context.stroke()
+        context.restore()
+    }
+    context.restore()
+}
