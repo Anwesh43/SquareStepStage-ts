a2f132702c09fe6f6f1ae38cae31e0da76da5e36
Anwesh43 <anweshthecool0@gmail.com>
2018-12-09 15:47:53 +0530

    adding constants

---
+++ SquareStackLineStepStage.ts
@@ -0,0 +1,7 @@
+const nodes : number = 5
+const lines : number = 5
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const color : string = "#01579B"
