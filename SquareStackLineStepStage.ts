63e7a2a17b1e608bc0a55370721372bf735cbc1b
Anwesh43 <anweshthecool0@gmail.com>
2018-12-09 16:27:42 +0530

    adding SquareStackLineStep class

--- SquareStackLineStepStage.ts
+++ SquareStackLineStepStage.ts
@@ -161,3 +161,25 @@ class SSLSNode {
         return this
     }
 }
+
+class SquareStackLineStep {
+    root : SSLSNode = new SSLSNode(0)
+    curr : SSLSNode = this.root
+    dir : number = 1
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
